793dd3842421e73266f5ddcf6350df7004073873
klemensle <klemensle@edu.aau.at>
2022-12-15 11:32:55 +0100

    Changes to LoginPage.js for redirection

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -1,46 +1,60 @@
-import React, { Component} from 'react';
-import { Col, Button, Form, FormGroup, Label, Input, Container } from 'reactstrap';
-
-class LoginPage extends Component {
-
-  state = {
-    credentials: {username: '', password: ''}
-  }
-
-  login = event => {
-    fetch('http://localhost:8005/api/v1/auth/', {
-      method: 'POST',
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify(this.state.credentials)
-    })
-    .then( data => data.json())
-    .then(
-        data => {
-            console.log(data.token)
-        }
-    )
-    .catch( error => console.error(error))
-  }
+import { Col, Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import { useState } from 'react';
 
 
-  inputChanged = event => {
-    const cred = this.state.credentials;
-    cred[event.target.name] = event.target.value;
-    this.setState({credentials: cred});
-  }
+const LoginPage = () => {
 
-  render() {
+    const [username, setUser] = useState('');
+    const [password, setPwd] = useState('');
+    const [success, setSuccess] = useState(false);
+
+    const login = async (e) => {
+        e.preventDefault();
+        fetch('http://localhost:8005/api/v1/auth/', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({username, password})
+        })
+        .then( data => data.json())
+        .then(
+            data => {
+                console.log(data.token)
+            }
+        )
+        .catch(( error => console.error(error)))
+        setUser('');
+        setPwd('');
+        setSuccess(true);
+    }
+
+
     return (
-      <div>
+        <>
+            {success ? (
+                <section>
+                    <h1>Thanks for logging in!</h1>
+                    <br/>
+                    <p>
+                        <a href='/'>Go back to Home</a>
+                    </p>
+                </section>
+            ) : (
+
+        <div>
         <h1>Login user form</h1>
         <hr className='mb-5'/>
         <Form>
         <FormGroup row>
           <Label className='col-sm-1'> Username: </Label>
           <Col sm={3}>
-            <Input type="text" name="username" 
-            value={this.state.credentials.username}
-            onChange={this.inputChanged}/>
+            <Input 
+                type="text" 
+                name="username"
+                autoComplete='off'
+                onChange={(e) => setUser(e.target.value)}
+                value={username}
+                required
+            />
           </Col>
         </FormGroup>
         <br/>
@@ -48,21 +62,24 @@ class LoginPage extends Component {
         <FormGroup row>
           <Label className='col-sm-1'> Password: </Label>
           <Col sm={3}>
-            <Input className='col-sm-3' type="password" name="password"
-            value={this.state.credentials.password}
-            onChange={this.inputChanged}/>
+          <Input 
+                type="password" 
+                name="password"
+                onChange={(e) => setPwd(e.target.value)}
+                value={password}
+                required
+            />
            </Col>
         </FormGroup>
 
         <br/>
-        <Button onClick={this.login}>Login</Button>
+        <Button onClick={login}>Login</Button>
       
         </Form>
       </div>
-
-
-    );
-  }
+      )}
+      </>
+    )
 }
 
-export default LoginPage;
+export default LoginPage

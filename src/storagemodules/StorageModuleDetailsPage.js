9fc53ab6a4e92566ef43a829b2e71d085adefb0a
JMahal0 <tajm27@gmail.com>
2023-01-23 05:18:17 +0100

    Implemented Storage Modules Details and List pages

--- src/storagemodules/StorageModuleDetailsPage.js
+++ src/storagemodules/StorageModuleDetailsPage.js
@@ -1,66 +1,29 @@
-import React, {useState} from 'react';
+import React from 'react';
 import StorageModuleForm from "./StorageModuleForm";
-import { Button, Container, Label } from 'reactstrap';
+import { Label } from 'reactstrap';
+import PropTypes from 'prop-types';
+import DetailsPage from '../components/DetailsPage';
 
+function StorageModuleDetailsPage({ token }) {
 
-function getIdNumber() {
-    let idPosition = 1 + window.location.pathname.lastIndexOf("/");
-    return Number(window.location.pathname.substring(idPosition));
-}
-
-function StorageModuleDetailsPage() {
-    const [isEditing, setIsEditing] = useState(false);
-
-    const storageModuleId = getIdNumber();
-    if (isNaN(storageModuleId)) {
-        return (
-            <h1>Invalid Page</h1>
-        );
-    }
-
-    //TODO: get this from a backend API call
-    let storageModule = {name: "a name", description: "a description", additionalInfo: "some additional info",
-     createdBy: "a createdby value"};
-
-    const handleDeleteButton = () => {
-        //TODO: call backend
-        alert("Implement API call to delete this System")
-    }
-
-    const displayDetails = () => {
+    function displayDetails(storageModule) {
         return <>
-
-            <Container className='row my-3'>
-            
             <Label>Description: {storageModule.description}</Label>
-            <Label>Additional Info:: {storageModule.additionalInfo}</Label>
-            <Label>CreatedBy: {storageModule.createdBy}</Label>
-            <Button className='my-2' onClick={() => setIsEditing(true)}>Edit</Button>
-            <Button  className='my-2' onClick={handleDeleteButton}>Delete</Button>
-            </Container>
+            <Label>File System Path: {storageModule.file_system_path}</Label>
         </>;
     };
 
-    const handleUpdateSubmitButton = () => {
-        //TODO: Call backend
-        alert("Implement API call to update this Storage Module");
-    };
-
-    const editingForm = () => {
-        return <>
-            <button onClick={() => setIsEditing(false)}>Cancel Edit</button>
-            <StorageModuleForm handleSubmit={handleUpdateSubmitButton} />
-        </>;
-    };
-
-    return (
-        <>
-            <h1>{storageModule.name}</h1>
-
-            {isEditing ? editingForm() : displayDetails()}
-
-        </>
-    );
+    return <DetailsPage
+            token={token}
+            apiPathSection={"storage-modules"}
+            displayDetails={displayDetails}
+            formComponent={<StorageModuleForm isAdd={false} token={token} />}
+           />;
 };
+
+
+StorageModuleDetailsPage.propTypes = {
+  token: PropTypes.string
+}
 
 export default StorageModuleDetailsPage;
